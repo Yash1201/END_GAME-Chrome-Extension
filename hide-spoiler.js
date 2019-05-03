7950f766452adc14fb1110a332c5a9c95a137878
Yash1201 <yashraj.50thecool@gmail.com>
2019-05-03 15:10:36 +0530

    first commit

---
+++ hide-spoiler.js
@@ -0,0 +1,48 @@
+kw = ['Gauntlet','spoiler alert end game','Avengers: Endgame','Avengers:Endgame','gauntlet','tony stark dead','Iron Man dead','Captain America','End Game','End Game Spoiler','Avengers End Game','Avengers End Game Spoiler','Dead','thanos dead','tony stark dead','iron man dead','end game','end game spoiler','avengers end game','avengers end game spoiler','dead','time travel']
+tags = "SPANEMBIULOLI";
+total = 0;
+
+for(var ii = 0; ii < kw.length; ii++)
+{
+	o = $(`:contains(${kw[ii]}):not(:has(:contains(${kw[ii]})))`)
+	for(var i = 0; i < o.length; i++)
+	{
+		if (!o[i].parentNode || o[i].parentNode.nodeName === "BODY") {
+          continue;
+        }
+		hideSpoiler(o[i]);
+		total++;
+	}
+}
+
+if(total >= 10) {
+	headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+	for(var i = 0; i < headings.length; i++) hideNode(headings[i]);
+}
+
+function hideSpoiler(node) {
+	ancestor = node.parentNode;
+	if(ancestor != null) {
+		if (ancestor.parentNode != null
+				&& ancestor.tagName != 'BODY')
+				ancestor = ancestor.parentNode;
+		imgs = ancestor.getElementsByTagName('img');
+		for(var i = 0; i < imgs.length; i++)
+			imgs[i].style.webkitFilter = "blur(10px)"
+		lists = ancestor.getElementsByTagName('li');
+		for(var i = 0; i < lists.length; i++) hideNode(lists[i]);
+	}
+
+	if (node == null || node.parentNode == null) return;
+	all_child = node.parentNode.children;
+	for(var i = 0; i < all_child; i++) {
+		var type = all_child[i].tagName;
+		if (tags.match(type) != null) hideNode(all_child[i]);
+	}
+	hideNode(node);
+}
+
+function hideNode(node) {
+	node.textContent = '[TEXT BLOCKED: SPOILER DETECTED]';
+	node.style.color = 'red'
+}
